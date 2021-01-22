2b92578225612f85851c661a9e031b3a0769b7bd
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-01-22 22:54:51 +0700

    Add files via upload

---
+++ recursion_factorial.py
@@ -0,0 +1,22 @@
+# get factorial with recursive function
+def factorial(num_param):
+	if num_param == 1:
+		return 1
+	else:
+		return (num_param * factorial(num_param - 1))
+
+# ask to run program more than once
+while True:
+	try:
+		num = int(input("Get factorial of : "))
+		print("The factorial of", num, "is", factorial(num))
+	except:
+		print("Input integer only!")
+		break
+
+	print("--------------------------------------------------")
+	again = str(input("Get factorial again ?(y/n) : "))
+	if again == "n" or again == "N":
+		break
+
+print("Thanks :)")
