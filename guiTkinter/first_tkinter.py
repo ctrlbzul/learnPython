4b762726a1a64acc7f135b40cc7c4ba7d919b3cb
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-01-25 00:42:15 +0700

    Update first_tkinter.py
    add some comments

--- guiTkinter/first_tkinter.py
+++ guiTkinter/first_tkinter.py
@@ -1,8 +1,10 @@
 # my first program with tkinter (python GUI)
 import tkinter as tk
 
+# create new windows (assigned to a_window)
 a_window = tk.Tk()
-txt = tk.Label(text = "MY FIRST TEXT IN TKINTER")
 
-txt.pack()
-a_window.mainloop()	
+txt = tk.Label(text = "DAYDREAMER, NIGHTTHINKER!") # create text(label) widget
+txt.pack() # add text widget to the window
+
+a_window.mainloop()	# loop event
