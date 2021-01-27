0bfbf443a10e87c57f5a8c237416a8cff53d2ff3
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-01-27 22:53:34 +0700

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,16 @@
+def reverse_string(my_string_param):
+	'''
+	In this particular example, the slice statement [::-1] means
+	start at the end of the string and end at position 0,
+	move with the step -1, negative one, which means one step backwards.
+	'''
+	return my_string_param[::-1]
+
+print('// STRING REVERSER //')
+print('----------------------------\n')
+
+# input string 
+my_string = str(input('Input the string\t: '))
+
+# print reversed string
+print('Reversed string\t\t:', reverse_string(my_string))
