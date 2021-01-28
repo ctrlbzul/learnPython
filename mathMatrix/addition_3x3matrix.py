902ebed21b17a96e8906c527572793e23e5e2dc4
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-01-28 19:47:44 +0700

    Add files via upload

---
+++ mathMatrix/addition_3x3matrix.py
@@ -0,0 +1,44 @@
+# Program to add two 3x3 matrices using nested loop
+# A matrix
+A = [[1, 2, 3],
+     [4, 5, 6],
+	   [7, 8, 9]]
+
+# B matrix
+B = [[9, 8, 7],
+     [6, 5, 4],
+	   [3, 2, 1]]
+
+# R matrix (A matrix + B matrix)
+R = [[0, 0, 0],
+	   [0, 0, 0],
+	   [0, 0, 0]]
+
+'''
+expected output:
+[10, 10, 10]
+[10, 10, 10]
+[10, 10, 10]
+'''
+
+# iterate through rows
+for i in range(len(A)):
+	# iterate through columns
+	for j in range(len(A[0])):
+		R[i][j] = A[i][j] + B[i][j]
+
+# print all matrices
+# Matrix A
+print('// MATRIX A //')
+for item in A:
+	print(item)
+
+# Matrix B
+print('\n// MATRIX B //')
+for item in B:
+	print(item)
+
+print('\nMATRIX R = MATRIX A + MATRIX B')
+print('// MATRIX R //')
+for item in R:
+	print(item)
