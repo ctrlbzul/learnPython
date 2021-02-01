5715b883dc39c302701e52f353b305f77d4ab765
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-01 12:32:32 +0700

    Add files via upload

---
+++ patternPrinting/pattern1.py
@@ -0,0 +1,29 @@
+# function to demonstrate printing pattern
+def pattern(n_param):
+
+	# outer i loop to handle number of rows (n)
+	for i in range(0, n_param):
+
+		# innet loop to handle number of columns
+		# values changing according to outer loop
+		for j in range(0, i+1):
+			# printing stars
+			print('*', end='')
+	
+		# linebreak after each 
+		print('\r')
+
+# take input
+n = int(input('Number of n\t: '))
+print('\n', end='') # linebreak
+# assign n to function
+pattern(n)
+
+'''
+expected output (n = 5):
+*
+**
+***
+****
+*****
+'''
---
+++ patternPrinting/pattern1_with_list.py
@@ -0,0 +1,21 @@
+# function to demonstrate printing pattern
+def pattern(n_param):
+	star_pattern = []
+	for i in range(1, n_param+1):
+		star_pattern.append('*' * i)
+	print('\n'.join(star_pattern))
+
+# take input
+n = int(input('Number of n\t: '))
+print('\n', end='') # linebreak
+# assign n to function
+pattern(n)
+
+'''
+expected output (n = 5):
+*
+**
+***
+****
+*****
+'''
