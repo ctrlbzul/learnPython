3f4853f4011b80971ca9783b6fcd5fdd2e36440a
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-05 22:38:11 +0700

    Add files via upload

---
+++ programizPythonExamples/add_two_numbers.py
@@ -0,0 +1,18 @@
+# WITHOUT USER INPUT
+
+# this program adds two numbers
+# Note: change the values for a different result
+num1 = 4.5
+num2 = 6.7
+
+# # to take the input from the user
+# num1 = input('Enter first number\t: ')
+# num2 = input('Enter second number\t: ')
+
+# adding two numbers
+sum = num1 + num2
+
+# display the sum
+print('{0} + {1} = {2}'.format(num1, num2, sum))
+
+
---
+++ programizPythonExamples/hello_world.py
@@ -0,0 +1,6 @@
+txt = 'Hello World, with a variable!'
+
+# print hello world without variable
+print('Hello World!')
+# print hello world with a variable (txt)
+print(txt)
