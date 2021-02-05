0fea1b60493265ab039d7634b661fe467011f205
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-05 22:48:26 +0700

    Add files via upload

---
+++ programizPythonExamples/find_square_root.py
@@ -0,0 +1,11 @@
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 16 
+
+# uncomment below to take the input from the user
+# num = float(input('Enter a number: '))
+
+# num_sqrt : square root of the num(ber)
+num_sqrt = num ** 0.5
+print('The square root of %i is %0.2f'%(num, num_sqrt))
