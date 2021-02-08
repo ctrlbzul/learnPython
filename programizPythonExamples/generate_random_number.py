27118439c6d48f193f2b3b4a4caa1d4e5efac12c
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-08 23:07:52 +0700

    Create generate_random_number.py

---
+++ programizPythonExamples/generate_random_number.py
@@ -0,0 +1,9 @@
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+# print a random number in range 0 to 5
+print('Random number\t:', random.randint(0,5))
+
+  
