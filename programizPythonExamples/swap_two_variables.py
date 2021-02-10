5f5a9b355d127a3931f85592c505fd3436ece0ff
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-10 22:24:04 +0700

    Update swap_two_variables.py

--- programizPythonExamples/swap_two_variables.py
+++ programizPythonExamples/swap_two_variables.py
@@ -1,1 +1,18 @@
+# python program to swap to variables
 
+x = 5
+y = 9
+# before swapping
+print('Before swapping\t: x = {0} and y = {1}'.format(x,y))
+
+# uncomment below to take the input from the user
+# x = input('Enter value of x\t: ')
+# y = input('Enter value of x\t: ')
+
+# create a temporary variable and swap the values
+temp = x # temp = 5
+x = y # x = y(9)
+y = temp # y = x(temp) = 5
+
+print('The value of x after swapping\t: {}'.format(x))
+print('The value of y after swapping\t: {}'.format(y))
