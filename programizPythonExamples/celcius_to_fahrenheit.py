ad684e145eaf194c5cf45a78f85b63f430be48b9
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-11 16:14:50 +0700

    Update celcius_to_fahrenheit.py

--- programizPythonExamples/celcius_to_fahrenheit.py
+++ programizPythonExamples/celcius_to_fahrenheit.py
@@ -1,1 +1,22 @@
+'''
+In the program below, we take a temperature in degree Celsius
+and convert it into degree Fahrenheit. They are related by the formula:
+celsius * 1.8 = fahrenheit - 32
+'''
 
+# change this value for a different result
+celcius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celcius * 1.8) + 32
+print('%0.1f degree Celcius is equal to %0.1f degree Fahrenheit' %(celcius, fahrenheit))
+
+# MODIFIED ABOVE PROGRAM : FAHRENHEIT TO CELCIUS
+# uncomment code below to convert fahrehnheit to celcius
+
+# # change this value for a different result
+# fahrenheit = 99.5
+
+# # calculate celcius
+# celcius = (fahrenheit - 32) / 1.8
+# print('%0.1f degree Fahrenheit is equal to %0.1f degree Celcius' %(fahrenheit,celcius))
