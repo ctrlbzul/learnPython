91139d3dd6dcedb0ca8c83c085c6a68eb7a249ec
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-11 17:52:17 +0700

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -4,9 +4,10 @@
 # from i = 1 to 10
 def mult_table(number_param):
 	for i in range(1, 11):
-		print(number_param, 'x', i, '=', number_param*i)
+		print('{0} x {1}\t= {2}'.format(number_param, i, (number_param*i)))
 
-print('// MULTIPLICATION TABLE 1-10 //\n')
+print('// MULTIPLICATION TABLE 1-10 //')
+print('-----------------------------------')
 try:
 	number = int(input('Display multiplication table of : '))
 	mult_table(number)
