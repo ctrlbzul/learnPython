af58e56829705b54f3bf59981874b90f0a5766cd
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-11 15:34:13 +0700

    Update kilometers_to_miles.py

--- programizPythonExamples/kilometers_to_miles.py
+++ programizPythonExamples/kilometers_to_miles.py
@@ -1,1 +1,22 @@
+# taking kilometers input from user
+kilometers = float(input('Enter value in kilometers\t: '))
 
+# conversion factor
+# 1 kilometer is equal to 0.621371 miles
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers, miles))
+
+# MODIFIED ABOVE PROGRAM : MILES TO KILOMETERS
+# uncomment code below to convert miles to kilometers
+# # taking miles input form user
+# miles = float(input('Enter value in miles\t: '))
+
+# # conversion factor
+# conv_fac = 0.621371
+
+# # calculate kilometers
+# kilometers = miles / conv_fac
+# print('%0.2f miles is equal to %0.2f kilometers' %(miles, kilometers))
