cc45cbaea8e9a51f92673565b2f33592098af54c
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-12 16:49:42 +0700

    Update wallet_management.py
    adding some comments and make the code cleaner.

--- wallet_management.py
+++ wallet_management.py
@@ -1,46 +1,64 @@
 class myWallet:
-  money = 0 #$ (dollar)
+	money = 0 # default amount of money
 
-  def set_money(self, set): # set (default) money
-    self.money = set
-    return self.money 
+	# function set money
+	def set_money(self, set):
+		self.money = set
+		return self.money 
 
-  def greet(self): # show current money
-    print('You HAVE', self.money, '$ in my wallet\n')
+	# function to show current money
+	def greet(self):
+		print('Money in your wallet\t: {0}$'.format(self.money))
 
-  def add_money(self, count):
-    self.money += count
-    print(count, '$ ADDED\nNow You have', self.money, '$ in my wallet')
+	# function to adding money
+	def add_money(self, count):
+		self.money += count
+		print('{0}$ ADDED\nNow You have {1}$ in your wallet'.format(count, self.money))
 
-  def take_money(self, money_taken):
-    if money_taken > self.money:
-      print('You DON\'T have that much money!')
-    elif money_taken == self.money:
-      print('Your wallet is now EMPTY')
-    else:
-      self.money -=  money_taken
-      print(money_taken, '$ TAKEN\nmoney in your wallet now :', self.money, '$')
+	# function to taking money
+	def take_money(self, money_taken):
+		if money_taken > self.money:
+			print('You DON\'T have that much money!')
+		elif money_taken == self.money:
+			print('Your wallet is now EMPTY')
+		else:
+			self.money -=  money_taken
+			print('{0}$ TAKEN\nmoney in your wallet now : {1}$'.format(money_taken, self.money))
 
-manage_money = myWallet() # make instance
+# function to print a straight line
+def printLine():
+	print('-----------------------------------')
 
-print('[ WALLET\'S\' MANAGEMENT ]\n')
-my_money = int(input('Set your money : '))
-print('MY MONEY :', manage_money.set_money(my_money), '$')
+# make an instance from myWallet class
+manage_money = myWallet()
 
-manage = input('Input m to manage money : ')
+print('[ WALLET MANAGEMENT ]')
+printLine()
+my_money = int(input('Set your money\t: '))
+print('MY MONEY\t: {0}$'.format(manage_money.set_money(my_money)))
+
+printLine()
+manage = input('Enter m to manage money\t: ')
 if manage == 'm' or manage == 'M':
-  manage_money.greet()
+	manage_money.greet()
+	printLine()
+	print('[ Manage Menu\t: 1. ADD MONEY  2. TAKE MONEY ]')
 
-  print('[ Manage Menu : 1. ADD MONEY  2. TAKE MONEY ]')
-  choose = input('input 1 or 2 : ')
-  if choose == '1':
-    add_count = int(input('Add money : '))
-    manage_money.add_money(add_count)
-  elif choose == '2':
-    take_count = int(input('Take money : '))
-    manage_money.take_money(take_count)
-  else:
-    print('You can manage your money anytime!')  
-
-print('\nThank You\n')
+	while(input != '1' or input != '2'):
+		choose = input('input 1 or 2\t: ')
+		if choose == '1':
+			add_count = int(input('Add money\t: '))
+			manage_money.add_money(add_count)
+			break
+		elif choose == '2':
+			take_count = int(input('Take money\t: '))
+			manage_money.take_money(take_count)
+			break
+		else:
+			print('Only input 1 or 2!')
+		printLine()  
+else:
+	print('You can manage your money anytime!')
+printLine()
+print('Thank You')
 
