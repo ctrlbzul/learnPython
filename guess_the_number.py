b83742946881163855a1e143dab4f6b85838f79a
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-12 23:13:18 +0700

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,12 +1,14 @@
+# import random modules
 import random
-
+# generate random number in range of 1 to 5
 random_number = random.randint(1, 5)
 
 while True:
-	guess = int(input("There is random number between 1 and 5,\nGUESS THE NUMBER : "))
+	print('There is random number between 1 and 5!')
+	guess = int(input("GUESS THE NUMBER : "))
 	if guess == random_number:
-		print("CORRECT! the random is", random_number)
+		print("CORRECT! the random number is {0}".format(random_number))
 		break
 	else:
-		print("Wrong, try again!")
+		print("WRONG, try again!")
 
