66d848918d5b28ab7abc92653b17d4cadf7eea62
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-12 21:50:27 +0700

    Update random_biodata.py

--- random_biodata.py
+++ random_biodata.py
@@ -5,8 +5,17 @@ import os
 # clear console
 os.system('clear')
 
-print('/ RANDOM BIODATA /')
+# function to print straight line
+def printLine():
+  print('-----------------------------------')
 
+# function to get age
+def get_age(born):
+	today = dt.date.today()
+	return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+print('// RANDOM BIODATA //')
+printLine()
 # generate random day, month, year
 random_day = rd.randint(1,31)
 random_month = rd.randint(1,12)
@@ -18,20 +27,16 @@ if random_day > 28:
 	random_month = rd.randint(3, 12)
 date_of_birth = dt.date(random_year, random_month, random_day)
 
-# function to get age
-def get_age(born):
-	today = dt.date.today()
-	return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-
-# data list
-names = ['Zill', 'Arthur', 'Aleister', 'Skud', 'Ormarr']
+# data lists
+names = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo']
 lives = ['California', 'Utah', 'Arizona', 'Wisconsin', 'Florida']
 random_date = [random_day, random_month, random_year]
 
-# print / generate random biodata
-print('Hi, My name is', rd.choice(names))
-print('I live in', rd.choice(lives))
+# print (generate) random biodata
+print('Hi, My name is {0}'.format(rd.choice(names)))
+print('I live in {0}'.format(rd.choice(lives)))
 print('Was born on ', end='') # end='' : to print in same line
 print(*random_date, sep='-')
-print('I am', get_age(date_of_birth), 'years old')
+print('I am {0} years old'.format(get_age(date_of_birth)))
+printLine()
 
