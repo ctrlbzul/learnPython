d4df99477e58484130cd85bcb78e978cad5cfe8b
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-12 22:42:17 +0700

    Update name_speller.py

--- name_speller.py
+++ name_speller.py
@@ -1,9 +1,17 @@
-print('[ NAME SPELLER ]\n')
+# function to print a straight line
+def printLine():
+  print('-----------------------------------')
 
-name = input('Input Name	: ')
-name2 = name.lower() # changing letter to lowercase
 
+print('[ NAME SPELLER ]')
+printLine()
+name = input('Input Name\t: ')
+final_name = name.lower() # changing letter to lowercase
+printLine()
+
+# get name length
 name_length = len(name)
+# get the spelling of each letter
 spellings = {'a':'ei', 'b':'bi', 'c':'si', 'd':'di', 'e':'i',
 						 'f':'ef', 'g':'ji', 'h':'eich', 'i':'ai', 'j':'jei',
 						 'k':'kei', 'l':'el', 'm':'em', 'n':'en', 'o':'ou',
@@ -11,11 +19,12 @@ spellings = {'a':'ei', 'b':'bi', 'c':'si', 'd':'di', 'e':'i',
 						 'u':'yu', 'v':'vi', 'w':'dablyu', 'x':'eks', 'y':'wai',
 						 'z':'zi', ' ':'space'}
 
-print('\nName lengths 	:', name_length, ' character (with space)')
-print('Name spellings	:')
-for letter in name2:
+print('Name lengths\t: {0} characters (with space)'.format(name_length))
+print('Name spellings\t:')
+for letter in final_name:
 	for spell in spellings:
-		if (spell == letter):
-			print(letter, ' = ', spellings[spell])
+		if spell == letter:
+			print('{0} = {1}'.format(letter, spellings[spell]))
 
-print('\nPROGRAM FINISHED')
+printLine()
+print('PROGRAM FINISHED')
