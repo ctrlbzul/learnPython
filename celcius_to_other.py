4933eb8dd58e907ad61e42de9fe2c7bbfd8a8b2b
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-13 23:28:34 +0700

    Update celcius_to_other.py

--- celcius_to_other.py
+++ celcius_to_other.py
@@ -1,22 +1,31 @@
+# degree symbol
 degree_sym = u'\N{DEGREE SIGN}' # degree symbol
+
+# function to print a straight line
+def printLine():
+	print('----------------------------------------')
+
+# function to convert celcius to reamur
 def toReamur(celcius_param):
-  print(celcius_param, (degree_sym + 'C'), ' = ', (4/5)*celcius_param, (degree_sym + 'R'), sep='')
+  print('{0}{1} = {2}{3}'.format(celcius_param, (degree_sym + 'C'), ((4/5)*celcius_param), (degree_sym + 'R')))
 
+# function to convert celcius to fahrenheit
 def toFahrenheit(celcius_param):
-  print(celcius_param, (degree_sym + 'C'), ' = ', celcius_param*(9/5) + 32, (degree_sym + 'F'), sep='')
+  print('{0}{1} = {2}{3}'.format(celcius_param, (degree_sym + 'C'), (celcius_param*(9/5) + 32), (degree_sym + 'F')))
 
+# function to convert celcius to kelvin
 def toKelvin(celcius_param):
-  print(celcius_param, (degree_sym + 'C'), ' = ', celcius_param + 273.15, (degree_sym + 'K'), sep='')
+  print('{0}{1} = {2}{3}'.format(celcius_param, (degree_sym + 'C'), (celcius_param + 273.15), (degree_sym + 'K')))
 
-print('/// CELCIUS CONVERTER ///')
+print('// CELCIUS CONVERTER //')
 print('1. Celcius to Reamur\n2. Celcius to Fahrenheit\n3. Celcius to Kelvin')
-print('/////////////////////////\n')
+printLine()
 
 while True:
-	# input celcius value
+	# get celcius value input
 	c_value = float(input('Input temperature in Celcius\t: '))
 
-	#convert the celcius value
+	# convert the celcius value
 	converter = input('Choose converter (1/2/3)\t: ')
 	if converter == '1':
 		toReamur(c_value)
@@ -27,8 +36,8 @@ while True:
 	else:
 		print('PLEASE CHOOSE BETWEEN 1-3!')
 
-	print("--------------------------------------------------")
-	again = str(input("Get factorial again ?(y/n)\t: "))
+	printLine()
+	again = str(input("Convert celcius again ?(y/n)\t: "))
 	if again == "n" or again == "N":
 		break
 
