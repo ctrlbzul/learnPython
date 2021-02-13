561f959fd67a45429568fef241ca78dd2a6387ec
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-14 06:40:15 +0700

    Update age_count.py

--- age_count.py
+++ age_count.py
@@ -1,15 +1,18 @@
-# import calendar module
+# import modules
 import calendar
-# import datetime class from datetime module
 from datetime import datetime
 
-# current date and time
+# function to print a straight line
+def printLine():
+	print('----------------------------------------')
+
+# get current date and time
 now = datetime.now()
 today = int(now.strftime("%d"))
 now_month = int(now.strftime("%m"))
 now_year = int(now.strftime("%Y"))
 
-# function to count age
+# function to count the age
 def count_age(day_input, month_input, year_input): 
   if day_input == today and month_input == now_month:
     your_age = now_year - year_input
@@ -24,10 +27,16 @@ def count_age(day_input, month_input, year_input):
     print("YOU ARE", your_age, "YEARS OLD.")
 
 day = int(input("Input your birth day (1-31): "))
-month = int(input("Innput your birth month (1-12) : "))
+month = int(input("Input your birth month (1-12) : "))
 year = int(input("Input your birth year : "))
 
-monthName = calendar.month_name[month] # get month name by an integer
-print(now.strftime('\nTODAY IS : %-d %B %Y'))
-print("YOUR BIRTHDAY IS : ON", day, monthName, year)
+# get month name by an integer
+monthName = calendar.month_name[month]
+
+# print the output
+printLine()
+print(now.strftime('TODAY IS : %-d %B %Y'))
+printLine()
+print("YOUR BIRTHDAY\t: {0} {1} {2}".format(day, monthName, year))
 count_age(day, month, year)
+printLine()
