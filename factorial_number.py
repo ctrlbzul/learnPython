0ae80cf6c67c16675ce41f27b6ff13f398728f87
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2021-02-13 11:39:49 +0700

    Update factorial_number.py

--- factorial_number.py
+++ factorial_number.py
@@ -2,7 +2,7 @@
 def factorialValue(num_param):
 	factorial = 1
 
-	# with while loops
+	# uncomment to use factorial function with while loops
 	# i = 1
 	# while i <= number:
 	# 	result*=i
@@ -15,16 +15,16 @@ def factorialValue(num_param):
 		print("0! = 1")
 	else:
 		for i in range(1, num_param+1): # factorial+1 for end value
-			factorial*=i
-			i+=1
-		print(num_param, "! =", factorial)
+			factorial *= i
+			# i += 1
+		print("{0}! = {1} (factorial of {2} is {3})".format(num_param, factorial, num_param, factorial))
 
 while True:
-	num = int(input("Input an integer number : ")) # num = number
+	num = int(input("Enter an integer number : "))
 	factorialValue(num)
-
-	# program's loop
-	again = str(input("Wanna use program again ? (y/n): "))
+	print('-----------------------------------')
+	# ask for program's loop
+	again = str(input("Use program again ? (y/n): "))
 	if again == "n" or again == "N" or again != "y":
 		break 
 
