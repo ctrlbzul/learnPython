0c366c578280adc5ee87cb78bce8e2f965a2bd03
Zulfikri Ramadhan Yoga <50226344+ctrlbzul@users.noreply.github.com>
2021-03-07 23:01:09 +0700

    Update factorial_number.py

--- factorial_number.py
+++ factorial_number.py
@@ -1,46 +1,18 @@
 # Python 3 program to find factorial of given number
 	
 # factorial is written with ! (ex : factorial of 5 = 5!)
+# factorial function with for loops
 def factorial(n_param):
-  # uncomment code below to use factorial function with recursive
-  # if n_param < 0:
-  #   return 0
-  # elif n_param == 0 or n_param == 1:
-  #   return 1
-  # else:
-  #   return n_param * factorial(n_param - 1)
+  fact = 1
 
-	# uncomment code below to use factorial function with while loops
-  # fact = 1
-  # i = 1
-  # while (i <= n_param):
-  #   fact *= i
-  #   i += 1
-  # return fact
-
-	# uncomment code below to use factorial function with iterative
-	# def factorial(n_param):
-	#   if n_param < 0:
-	#     return 0
-	#   elif n_param == 0 or n_param == 1:
-	#     return 1
-	#   else:
-	#     fact = 1
-	#     while (n_param > 1):
-	#       fact *= n_param
-	#       n_param -=1
-	#     return fact
-
-	# uncomment code below to use factorial function with for loops
-	# if n_param < 0:
-	# 	return 0
-	# elif n_param == 0 or n_param == 1:
-	# 	return 1
-	# else:
-	# 	for i in range(1, n_param+1): # factorial+1 for end value
-	# 		fact *= i
-	# 		# i += 1
-	# 	return fact
+  if n_param < 0:
+    return 0
+  elif n_param == 0 or n_param == 1:
+    return 1
+  else:
+    for i in range(1, n_param + 1):
+      fact *= i
+    return fact
 
 while True:
 	# given number
