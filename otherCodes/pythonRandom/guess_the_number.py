3cc8c586e9a8bbb48900ae457f7f49a4edaab407
Zulfikri Ramadhan Yoga <50226344+ctrlbzul@users.noreply.github.com>
2021-03-18 20:24:21 +0700

    Update guess_the_number.py

--- otherCodes/pythonRandom/guess_the_number.py
+++ otherCodes/pythonRandom/guess_the_number.py
@@ -4,6 +4,16 @@ def printLine():
 	# function : print a straight line
 	print('--------------------------------------------------')
 
+# def isDigit(check_input):
+# 	'''
+# 	function : check if input is integers or not
+# 	return : bool
+# 	'''
+# 	if check_input.isdigit():
+# 		return True
+# 	return False
+
+
 def computerNumber(min_num, max_num):
 	'''
 	function : generates a random number based on the args (min_num, max_num).
@@ -16,45 +26,45 @@ def playerGuess(min_num, max_num):
 	function : asks the player to guess a number.
 	return user_input
 	'''
-	# user_input = int(input(f'Guess the number between {min_num} and {max_num} : '))
-	# check for user errors
+	user_input = input(f'Guess the number between {min_num} and {max_num} : ')
+
+	# check for user input error
 	while True:
-		try:
-			user_input = int(input(f'Guess the number between {min_num} and {max_num} : '))
+		if user_input.isdigit():
 			# check if input is an integer and within the range
-			if isinstance(user_input, int) and user_input in range(min_num, max_num+1):
+			if int(user_input) in range(min_num, max_num + 1):
 				break
-			elif user_input not in range(min_num, max_num + 1):
-				user_input = int(input(f'Please, input the number between {min_num} and {max_num} : '))
-		except:
-			user_input = int(input(f'Please, input integer only : '))
+			elif int(user_input) not in range(min_num, max_num + 1):
+				user_input = input(f'Please, input the number between {min_num} and {max_num} : ')
+		else:
+			print(f'Error : please input an integer!')
+			user_input = input(f'Guess the number between {min_num} and {max_num} : ')
 			
-	return user_input
+	input_result = int(user_input)
+	return input_result
 
-def play():
-	print('>> GUESS THE NUMBER')
-	printLine()
+# MAIN CODE
+print('>> GUESS THE NUMBER')
+printLine()
 
-	# define guessing range
-	low = 0
-	high = 5
-	# if player wanna decide the guessing range.
-	# low = int(input('Input low guessing range : '))
-	# high = int(input('Input high guessing range : '))
+# define guessing range
+low = 0
+high = 5
 
-	computer_choice = computerNumber(low, high)
-	player_choice = playerGuess(low, high)
+# if player wanna decide the guessing range.
+# low = int(input('Input low guessing range : '))
+# high = int(input('Input high guessing range : '))
 
-	# loop until player guesses the right number.
-	while computer_choice != player_choice:
-		if player_choice > computer_choice:
-			player_choice = int(input('Number is too high, try again\t: '))
-		elif player_choice < computer_choice:
-			player_choice = int(input('Number is too low, try again\t: '))
+computer_choice = computerNumber(low, high)
+player_choice = playerGuess(low, high)
 
-	# if player guesses the right number
-	printLine()
-	print(f'CONGRATULATIONS! You guess the right number : {computer_choice}')
+# loop until player guesses the right number.
+while computer_choice != player_choice:
+	if player_choice > computer_choice:
+		player_choice = int(input('Number is too high, try again\t: '))
+	elif player_choice < computer_choice:
+		player_choice = int(input('Number is too low, try again\t: '))
 
-# start the guessing game
-play()
+# if player guesses the right number
+printLine()
+print(f'CONGRATULATIONS! You guess the right number : {computer_choice}')
