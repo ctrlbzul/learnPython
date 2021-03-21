4ee3bd92edefeaec535f4fee6212efc91b45eb7b
Zulfikri Ramadhan Yoga <50226344+ctrlbzul@users.noreply.github.com>
2021-03-21 11:27:30 +0700

    Update check_input.py

--- otherCodes/check_input.py
+++ otherCodes/check_input.py
@@ -1,73 +1,30 @@
-from random import randint
+# function : check is input an integer or not
+def isInteger(check_input):
+  if check_input.isdigit():
+    return True
+  return False
 
-def printLine():
-	# function : print a straight line
-	print('--------------------------------------------------')
+# function : check is input a string or not
+def isString(check_input):
+  if check_input.isalpha():
+    return True
+  return False
 
-def computerNumber(min_num, max_num):
-	'''
-	function : generates a random number based on the args (min_num, max_num).
-	return random int
-	'''
-	return randint(min_num, max_num)
+# function : check is input a float or not
+def isFloat(check_input):
+  if '.' in check_input:
+    split_input = check_input.split('.')
+    if len(split_input) == 2 and split_input[0].isdigit() and split_input[1].isdigit():
+      return True
+  return False
 
-def playerGuess(min_num, max_num):
-	'''
-	function : asks the player to guess a number.
-	return user_input
-	'''
-	user_input = input(f'Guess the number between {min_num} and {max_num} : ')
-
-	# check for user input error
-	while True:
-		# check if input is an integer and within the range
-		if user_input.isdigit():
-			if int(user_input) in range(min_num, max_num + 1):
-				break
-			elif int(user_input) not in range(min_num, max_num + 1):
-				user_input = input(f'Please, input the number between {min_num} and {max_num} : ')
-		else:
-			print(f'Error : please input an integer!')
-			user_input = input(f'Guess the number between {min_num} and {max_num} : ')
-			
-	input_result = int(user_input)
-	return input_result
-
-# MAIN CODE
-print('>> GUESS THE NUMBER')
-printLine()
-
-# define guessing range
-low = 0
-high = 7
-
-# if player wanna decide the guessing range.
-# low = int(input('Input low guessing range : '))
-# high = int(input('Input high guessing range : '))
-
-computer_choice = computerNumber(low, high)
-player_choice = playerGuess(low, high)
-
-# loop until player guesses the right number.
-# while computer_choice != player_choice:
-# chance
-tired = 0
-while True:
-	# if guessed number wrong 3 times, ask for quit
-	if tired == 3:
-		give_up = input('Tired of guessing ? Wanna quit ? (y/n) : ')
-		if give_up == 'y' or give_up == 'Y':
-			print(f'You are giving up. The right number is {computer_choice}.')
-			break
-
-	if player_choice > computer_choice:
-		player_choice = int(input('Number is too high, try again\t: '))
-		tired += 1
-	elif player_choice < computer_choice:
-		player_choice = int(input('Number is too low, try again\t: '))
-		tired += 1
-	else:
-		# if player guesses the right number, out from while loop
-		printLine()
-		print(f'CONGRATULATIONS! You guess the right number : {computer_choice}.')
-		break
+# user code
+user_input = input('Type anything : ')
+if isInteger(user_input):
+  print(f'Your input : {user_input}, is an integer.')
+elif isString(user_input):
+  print(f'Your input : {user_input}, is a string.')
+elif isFloat(user_input):
+  print(f'Your input : {user_input}, is a float.')
+else:
+  print(f'Your input : {user_input}, is a string.')
