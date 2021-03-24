5bd7f1acaff8b2309b7ca984ccd561a120aaea8a
Zulfikri Ramadhan Yoga <50226344+ctrlbzul@users.noreply.github.com>
2021-03-24 20:47:30 +0700

    Update kuis_peribahasa.py

--- kuis_peribahasa.py
+++ kuis_peribahasa.py
@@ -9,34 +9,46 @@ def nomorSoal():
 	return random.randint(1,3)
 
 # list daftar soal
-daftar_soal = ['Peribahasa yang memiliki makna\'kebaikan dibalas keburukan\' adalah...',
+daftar_soal = ['Peribahasa yang memiliki makna \'kebaikan dibalas keburukan\' adalah...',
 							 'Makna peribahasa \'bersatu kita teguh bercerai kita runtuh\' adalah...',
 							 'Makna peribahasa \'besar pasak daripada tiang\'adalah...']
 
-# list daftar jawaban
-daftar_jawaban = ['a. Air cucuran atap jatuhnya ke pelimbahan juga.\nb. Air susu dibalas dengan air tuba.\nc. Angkuh terbawa tampan tertinggal.',
-									'a. Jika kita bersatu kita tidak akan runtuh.\nb. Sesuatu yang sebaiknya kita lakukan bersama.\nc. Sesuatu akan berhasil juga dilakukan secara bersama-sama.',
-									'a. Hal yang tidak mungkin dilakukan.\nb. Pengeluaran lebih besar daripada pendapatan.\nc. Sesuatu yang tidak berguna dan bermanfaat.']
+jawabanSoal1 = ['Air cucuran atap jatuhnya ke pelimbahan juga.',
+								'Air susu dibalas dengan air tuba.',
+								'Angkuh terbawa tampan tertinggal.']
+
+jawabanSoal2 = ['Jika kita bersatu kita tidak akan runtuh.',
+								'Sesuatu yang sebaiknya kita lakukan bersama.',
+								'Sesuatu akan berhasil juga dilakukan secara bersama-sama.']
+
+jawabanSoal3 = ['Hal yang tidak mungkin dilakukan.',
+								'Pengeluaran lebih besar daripada pendapatan.',
+								'Sesuatu yang tidak berguna dan bermanfaat.']
+
+huruf_kuis = ['a', 'b', 'c']
 
 # set soal dan jawaban
 nomor = nomorSoal()
 if nomor == 1:
 	soal = daftar_soal[0]
-	jawaban = daftar_jawaban[0]
+	jawaban = jawabanSoal1
 	kunci = 'b'
 elif nomor == 2:
 	soal = daftar_soal[1]
-	jawaban = daftar_jawaban[1]
+	jawaban = jawabanSoal2
 	kunci = 'c'
 elif nomor == 3:
 	soal = daftar_soal[2]
-	jawaban = daftar_jawaban[2]
+	jawaban = jawabanSoal3
 	kunci = 'b'
 
 # generate soal untuk user
 print(f'/ KUIS PERIBAHASA /')
 printLine()
-print(f'{soal}\n{jawaban}')
+print(f'{soal}')
+posisi_jawaban = random.sample(jawaban, 3)
+for huruf, jawaban in zip(huruf_kuis, posisi_jawaban):
+	print(f'{huruf}. {jawaban}')
 
 # jawaban dari user
 jawaban_user = input('Jawaban anda : ')
