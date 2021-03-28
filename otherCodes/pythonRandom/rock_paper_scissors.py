b2c94809f6129b21de4dc7518263c688955263d1
Zulfikri Ramadhan Yoga <50226344+ctrlbzul@users.noreply.github.com>
2021-03-28 22:56:50 +0700

    Update rock_paper_scissors.py

--- otherCodes/pythonRandom/rock_paper_scissors.py
+++ otherCodes/pythonRandom/rock_paper_scissors.py
@@ -1,8 +1,11 @@
 from random import randint
 
+# function : print a straight line
 def printLine():
   return(f'---------------------------------------------')
 
+'''function : show options and ask for player choice
+return int'''
 def userInterface(options):
   for index, option in enumerate(options):
     print(f'{index} = {option}')
@@ -10,9 +13,21 @@ def userInterface(options):
   user_input = int(input('What do u choose ? '))
   return user_input
 
+'''function : generate a random number for computer choice
+return random int'''
 def computerChoice(content):
   computer_chose = randint(0, len(content)-1)
   return computer_chose
+
+'''function : check who won
+return string'''
+def checkResults(choices, player, computer):
+  if player == computer:
+    return 'It\'s a tie.'
+  elif (player == 0 and computer == len(choices)-1) or (player > computer and not(player == len(choices)-1 and computer == 0)):
+    return 'Player WON, Computer LOST'
+  return 'Player LOST, Computer WON'
+
 
 def play():
   print(f'''{printLine()}\nWelcome to Rock, Paper, Scissors.\nPlease pick your weapon.''')
@@ -27,4 +42,16 @@ def play():
   print(f'Player chose\t: {options_list[player_result]}')
   print(f'Computer chose\t: {options_list[computer_result]}')
 
-play()
+  # check the results and print the winner
+  results = checkResults(options_list, player_result, computer_result)
+  print(f'{printLine()}\n{results}')
+
+def main():
+  # force player into the play loop
+  play_again = ''
+  while play_again.lower() != 'n':
+    play()
+    print(f'{printLine()}\nDo u want to play again ?')
+    play_again = input('type \'y\' for yes or \'n\' for no : ')
+
+main()
